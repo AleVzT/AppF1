242bad0b8409c55acd1bce848d55bc80668b225a
agarcialg66@gmail.com <sisesrltech@gmail.com>
2025-03-18 17:24:37 -0300

    Se termina la vista de listado de pilotos

---
+++ src/app/pages/pilot-list/pilot-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { DriverResponse, TeamModel } from 'src/app/models';
+import { F1ApiService } from 'src/app/services/f1-api.service';
+
+@Component({
+  selector: 'app-pilot-list',
+  templateUrl: './pilot-list.component.html',
+  styleUrls: ['./pilot-list.component.scss']
+})
+export class PilotListComponent  implements OnInit {
+  selectedEscuderia = 'mercedes';
+
+  escuderias: TeamModel[] = [];
+
+  teamName: string = '';
+
+  pilots: DriverResponse[] = []
+
+  constructor(private f1ApiService: F1ApiService) { }
+
+  ngOnInit(): void {
+    this.handleAllTeams();
+    if(this.selectedEscuderia) {
+      this.HandleDrivers();
+    }
+  }
+
+  HandleDrivers(): void {
+    this.f1ApiService.getDriversByTeams(this.selectedEscuderia).subscribe(data => {
+      this.pilots = data.drivers;
+      this.teamName = data.team.teamName;
+    });
+  }
+
+  handleAllTeams(): void {
+    this.f1ApiService.getTeams().subscribe(data => {
+      this.escuderias = data.teams;
+    });
+  }
+
+  changeEscuderia() {
+    this.HandleDrivers();
+  }
+}
